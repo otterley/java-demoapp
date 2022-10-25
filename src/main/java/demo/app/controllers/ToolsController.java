d54c5ba9cb69de61e048d86f18cb27918dda9c25
Michael Fischer <fiscmi@amazon.com>
2022-10-25 19:24:35 +0800

    Burn CPU for a shorter time to make load tests better

--- src/main/java/demo/app/controllers/ToolsController.java
+++ src/main/java/demo/app/controllers/ToolsController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public final class ToolsController {
   /**
    * CheckStyle forced me to add all these comments.
+   * 
    * @return Who cares
    */
   static final int MAX_ERR = 6;
 
   /**
    * Tools page.
+   * 
    * @return The named view
    */
   @GetMapping("/tools")
@@ -26,6 +28,7 @@ public final class ToolsController {
 
   /**
    * Force a fake error page.
+   * 
    * @return The named view
    */
   @GetMapping("/tools/error")
@@ -35,40 +38,35 @@ public final class ToolsController {
     switch (randNum) {
       case 0:
         throw new NullPointerException(
-                "Oh that thing you liked? It's gone"
-              );
+            "Oh that thing you liked? It's gone");
       case 1:
         throw new MalformedURLException(
-                "URL has too many gerbils"
-              );
+            "URL has too many gerbils");
       case 2:
         throw new ArrayIndexOutOfBoundsException(
-                "Wow I thought that array was bigger"
-              );
+            "Wow I thought that array was bigger");
       case 3:
         throw new SQLException(
-                "Permission denied for table LEMON_CURD"
-              );
+            "Permission denied for table LEMON_CURD");
       case 4:
         throw new StackOverflowError(
-                "Stack overflow! I mean it, a real one, not the website"
-              );
+            "Stack overflow! I mean it, a real one, not the website");
       default:
         throw new FileNotFoundException(
-                "File unexpectedly turned into cheese"
-              );
+            "File unexpectedly turned into cheese");
     }
   }
 
   /**
    * Force a CPU load spike.
+   * 
    * @param model something
    * @return The named view
    */
   @GetMapping("/tools/cpu")
   public String cpuLoad(final Model model) {
     long start = java.lang.System.currentTimeMillis();
-    for (int i = 0; i < 499900000.0; i++) {
+    for (int i = 0; i < 100000000.0; i++) {
       Math.pow(9000.0, 9000.0);
     }
     long elapsed = java.lang.System.currentTimeMillis() - start;
